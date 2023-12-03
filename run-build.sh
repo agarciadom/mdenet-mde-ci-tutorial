e3b0e5d919066e1411bd1b4ab61573ab90850493
Antonio García-Domínguez <a.garcia-dominguez@york.ac.uk>
2023-12-03 14:32:40 +0000

    run-build.sh: pull latest image

--- run-build.sh
+++ run-build.sh
@@ -2,9 +2,10 @@
 
 set -e
 
-docker run --rm -v .:/code \
-  registry.gitlab.com/committed-consulting/mde-devops/epsilon-ci-container:latest \
-  /code/build.xml main
+DOCKER_IMAGE=registry.gitlab.com/committed-consulting/mde-devops/epsilon-ci-container:latest
+
+docker pull "$DOCKER_IMAGE"
+docker run --rm -v .:/code "$DOCKER_IMAGE" /code/build.xml main
 
 pushd gradle
 ./gradlew build
